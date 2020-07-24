0c73bc8f6715e7ddf5c33bfc0c63ed4c7c85c5ca
abena1apaw <57848351+abena1apaw@users.noreply.github.com>
2020-07-23 19:45:20 -0500

    created blog site

---
+++ blog/blog/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Router from './components/Router';
+import Footer from "./Layout/Footer";
+ 
+class App extends Component {
+  render() {
+    return (
+        <Router />
+    );
+  }
+}
+ 
+export default App;
