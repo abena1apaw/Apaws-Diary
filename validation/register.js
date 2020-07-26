28ceb26428f3907b94730735c08e37428f67f117
Abena Apaw <57848351+abena1apaw@users.noreply.github.com>
2020-07-26 10:44:00 -0500

    Update register.js

--- validation/register.js
+++ validation/register.js
@@ -2,7 +2,7 @@ const Validator = require("validator");
 const isEmpty = require("is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
-// Convert empty fields to an empty string so we can use validator functions
+
   data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
@@ -28,7 +28,7 @@ if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
   }
 if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
+    "Passwords must match";
   }
 return {
     errors,
